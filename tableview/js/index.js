90d2d1368c65ee78f5408c0473a5944028a23cc3
sean-7777 <29sxiao@gmail.com>
2022-10-02 16:56:06 +0000

    Deploying to pages from @ seven-teams/seven-teams.github.io@2f7887bad23ed1819cabd669f0c7033a703d54f9 🚀

--- tableview/js/index.js
+++ tableview/js/index.js
@@ -8,37 +8,52 @@ import {
   FormatModule,
 } from "../lib/tabulator_esm.min.js";
 
-Tabulator.registerModule([
-  AjaxModule,
-  FilterModule,
-  EditModule,
-  SortModule,
-  FormatModule,
-  PopupModule,
-]);
+try {
+  Tabulator.registerModule([
+    AjaxModule,
+    FilterModule,
+    EditModule,
+    SortModule,
+    FormatModule,
+    PopupModule,
+  ]);
 
-const params = new Proxy(new URLSearchParams(window.location.search), {
-  get: (searchParams, query) => searchParams.get(query),
-});
+  const params = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, query) => searchParams.get(query),
+  });
 
-const columns = await (await fetch(params.columns)).json();
+  const columns = await (await fetch(params.columns)).json();
+  const r = await (await fetch(params.data)).json();
 
-new Tabulator(".table", {
-  height: "100vw",
-  columns: Object.entries(columns).map(([name, cfgs]) => ({
-    title: cfgs.title || name,
-    field: name,
-    headerFilter: cfgs?.filter?.type,
-    headerFilterParams: cfgs?.filter?.params,
-    sorter: cfgs?.sorter?.type,
-    sorterParams: cfgs?.sorter?.params,
-    formatter: cfgs?.format,
-    clickPopup: cfgs?.popup
-      ? (event, component, onRendered) => component.getInitialValue()
-      : undefined,
-    formatter: cfgs?.popup
-      ? (cell, params, onRendered) => cell.getValue().split(" ").slice(0, 8).join(" ")
-      : undefined,
-  })),
-  ajaxURL: params.data,
-});
+  new Tabulator(".table", {
+    height: "100vw",
+    columns: Object.entries(columns).map(([name, cfgs]) => ({
+      title: cfgs.title || name,
+      field: name,
+
+      headerFilter: cfgs?.filter?.type,
+      headerFilterParams: cfgs?.filter?.params,
+
+      sorter: cfgs?.sorter?.type,
+      sorterParams: cfgs?.sorter?.params,
+      headerSort: cfgs?.sorter?.enable || false,
+      headerSortTristate: true,
+
+      clickPopup: cfgs?.popup?.enable
+        ? (event, component, onRendered) => component.getInitialValue()
+        : undefined,
+      formatter: cfgs?.popup?.enable
+        ? (cell, params, onRendered) =>
+            cell
+              .getValue()
+              .split(cfgs?.popup?.delimiter || " ")
+              .slice(0, cfgs?.popup?.count || 8)
+              .join(cfgs?.popup?.delimiter || " ")
+        : cfgs?.format,
+    })),
+    ajaxURL: params.data,
+  });
+} catch (error) {
+  document.querySelector(".error").innerText = error.stack;
+  console.log(error.stack);
+}
