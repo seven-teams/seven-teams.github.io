64599f2539718e4f03aab669840b8b523578d85b
sean-7777 <29sxiao@gmail.com>
2022-10-09 17:09:23 +0000

    Deploying to pages from @ seven-teams/seven-teams.github.io@43184a9b0a893772c01e625f7706adc87d97fd9e 🚀

--- tableview/js/index.js
+++ tableview/js/index.js
@@ -7,9 +7,15 @@ import {
   PopupModule,
   FormatModule,
   MenuModule,
-} from "../lib/tabulator_esm.min.js";
+} from "https://unpkg.com/tabulator-tables/dist/js/tabulator_esm.min.js";
 
 try {
+  function removeUndefined(obj) {
+    return Object.fromEntries(
+      Object.entries(obj).filter(([_, val]) => val !== undefined)
+    );
+  }
+
   const showColDiv = document.querySelector(".show");
   function createShowButton(column) {
     let showButton = document.createElement("button");
@@ -36,60 +42,64 @@ try {
   });
 
   const columns = await (await fetch(params.columns)).json();
+  const tab = new Tabulator(".table", {
+    // height: document.documentElement.clientHeight,
+    columns: Object.entries(columns).map(([name, cfgs]) =>
+      removeUndefined({
+        title: cfgs.title || name,
+        field: name,
 
-  new Tabulator(".table", {
-    height: "100vw",
-    columns: Object.entries(columns).map(([name, cfgs]) => ({
-      title: cfgs.title || name,
-      field: name,
+        headerFilter: cfgs?.filter?.type,
+        headerFilterParams: cfgs?.filter?.params,
 
-      headerFilter: cfgs?.filter?.type,
-      headerFilterParams: cfgs?.filter?.params,
+        sorter: cfgs?.sorter?.type,
+        sorterParams: cfgs?.sorter?.params,
+        headerSort: cfgs?.sorter?.enable || false,
+        headerSortTristate: true,
 
-      sorter: cfgs?.sorter?.type,
-      sorterParams: cfgs?.sorter?.params,
-      headerSort: cfgs?.sorter?.enable || false,
-      headerSortTristate: true,
+        visible: cfgs?.visiblity,
 
-      visible: cfgs?.visiblity,
+        clickPopup:
+          cfgs?.popup?.enable &&
+          ((event, component, onRendered) => component.getInitialValue()),
+        formatter: cfgs?.popup?.enable
+          ? (cell, params, onRendered) =>
+              cell
+                .getValue()
+                .split(cfgs?.popup?.delimiter || " ")
+                .slice(0, cfgs?.popup?.count || 8)
+                .join(cfgs?.popup?.delimiter || " ")
+          : cfgs?.format,
+        formatterParams:
+          cfgs?.format === "link" && cfgs?.formatParams?.labelField
+            ? {
+                ...cfgs?.formatParams,
+                labelField: undefined,
+                label: (cell) =>
+                  cell
+                    .getRow()
+                    .getCell(cfgs.formatParams.labelField)
+                    .getValue() || cell.getValue(),
+              }
+            : cfgs?.formatParams,
 
-      clickPopup: cfgs?.popup?.enable
-        ? (event, component, onRendered) => component.getInitialValue()
-        : undefined,
-      formatter: cfgs?.popup?.enable
-        ? (cell, params, onRendered) =>
-            cell
-              .getValue()
-              .split(cfgs?.popup?.delimiter || " ")
-              .slice(0, cfgs?.popup?.count || 8)
-              .join(cfgs?.popup?.delimiter || " ")
-        : cfgs?.format,
-      formatterParams:
-        cfgs?.format === "link" && cfgs?.formatParams?.labelField
-          ? {
-              ...cfgs?.formatParams,
-              labelField: undefined,
-              label: (cell) =>
-                cell
-                  .getRow()
-                  .getCell(cfgs.formatParams.labelField)
-                  .getValue() || cell.getValue(),
-            }
-          : cfgs?.formatParams,
-
-      headerMenu: cfgs?.menu && [
-        cfgs?.menu?.hideable && {
-          label: "Hide Column",
-          action: (evt, column) => {
-            column.hide();
-            createShowButton(column);
+        headerMenu: cfgs?.menu && [
+          cfgs?.menu?.hideable && {
+            label: "Hide Column",
+            action: (evt, column) => {
+              column.hide();
+              createShowButton(column);
+            },
           },
-        },
-      ],
-      headerMenuIcon: cfgs?.menu && `<span style="line-height: 1em">☰</span>`,
-    })),
+        ],
+        headerMenuIcon: cfgs?.menu && `<span style="line-height: 1em">☰</span>`,
+      })
+    ),
     ajaxURL: params.data,
+    // data: await (await fetch(params.data)).json(),
   });
+
+  tab.on("tableBuilt", () => console.log(tab.getColumnDefinitions()));
 } catch (error) {
   document.querySelector(".error").innerText = error.stack;
   console.log(error.stack);
